e6a4a06703749cf04b652f0fa58ee60d6c167613
chakri <aryangajvelli2011@gmail.com>
2016-11-24 15:02:14 +0530

    New UI added, event handlers yet to be coded

--- src/primary_ui.py
+++ src/primary_ui.py
@@ -1,25 +1,30 @@
 import sys
 from PyQt4.QtCore import *
+from PyQt4.QtCore import QObject
 from PyQt4.QtGui import *
+from view_details import view_items
+from view_bills import view_bills
 
 
 def window():
    app = QApplication(sys.argv)
    win = QDialog()
    b1 = QPushButton(win)
-   b1.setText("Button1")
+   b1.setText("View Items")
    b1.move(50,20)
-   b1.clicked.connect(b1_clicked)
+   # b1.clicked.connect(view_items) # for python 3
+   QObject.connect(b1,SIGNAL("clicked()"), view_items)
 
    b2 = QPushButton(win)
-   b2.setText("Button2")
+   b2.setText("View Bills")
    b2.move(50,50)
-   QObject.connect(b2,SIGNAL("clicked()"),b2_clicked)
-
+   # b2.clicked.connect(view_bills) # for python 3
+   QObject.connect(b2, SIGNAL("clicked()"), view_bills)
    win.setGeometry(100,100,200,100)
-   win.setWindowTitle("PyQt")
+   win.setWindowTitle("S market")
    win.show()
    sys.exit(app.exec_())
+
 
 def b1_clicked():
    print "Button 1 clicked"
